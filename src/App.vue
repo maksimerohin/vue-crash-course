c7c7452c449013cb9b16a91bd3c0ea7deb10f952
Maksim Erokhin <maksimerohin@gmail.com>
2020-08-16 20:16:09 +0700

    Add server data

--- src/App.vue
+++ src/App.vue
@@ -20,12 +20,15 @@
         name: 'App',
         data() {
             return {
-                todos: [
-                    {id: 1, title: 'Купить хлеб', completed: false},
-                    {id: 2, title: 'Купить масло', completed: false},
-                    {id: 3, title: 'Купить сыр', completed: false}
-                ]
+                todos: []
             }
+        },
+        mounted() {
+            fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
+                .then(response => response.json())
+                .then(json => {
+                    this.todos = json
+                })
         },
         methods: {
             removeTodo(id) {
